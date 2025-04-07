96886f8331ea5fec8df5ab3b2bb4a695a15de2ea
Yash <118760780+Iamyashsiwach@users.noreply.github.com>
2025-04-08 01:13:32 +0530

    Updated cv

--- app/components/ui/floating-navbar.tsx
+++ app/components/ui/floating-navbar.tsx
@@ -74,7 +74,7 @@ import Link from "next/link";
     <span className="hidden sm:block text-sm">{navItem.name}</span>
   </Link>
 ))}
-        <a href="https://drive.google.com/file/d/1r6HOIzGNlB14osognfR7tnturkDGEqyi/view?usp=sharing" target="_blank">
+        <a href="https://drive.google.com/file/d/1tGy3s4sajH-kB7quN0ibmJJxxyzJCDZg/view?usp=sharing" target="_blank">
         <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
           <span>Resume</span>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
